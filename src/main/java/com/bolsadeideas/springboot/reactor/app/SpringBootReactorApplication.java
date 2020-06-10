fa115baa795759cb3bd6156a7c824c33d054106b
jcmartinez <jcmartinezcu@gmail.com>
2020-06-10 16:30:52 +0200

    video 362

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bolsadeideas.springboot.reactor.app.models.Usuario;
+
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
@@ -20,15 +22,21 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		Flux<String> nombres = Flux.just("Andres", "Pedro", "Maria", "Diego", "Juan")
-				.doOnNext( e->{
-					if(e.isEmpty()) {
+		Flux<Usuario> nombres = Flux.just("Andres", "Pedro", "Maria", "Diego", "Juan")
+				.map(nombre ->  new Usuario(nombre.toUpperCase(), null))
+				.doOnNext( usuario->{
+					if(usuario == null) {
 						throw new RuntimeException("Nombres no pueden ser vacios");
 					}
-					System.out.println(e);
+					System.out.println(usuario.getNombre());
+				})
+				.map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
 				});
 		
-		nombres.subscribe(log::info,
+		nombres.subscribe(e -> log.info(e.toString()),
 				error -> log.error(error.getMessage()),
 				new Runnable() {
 					
